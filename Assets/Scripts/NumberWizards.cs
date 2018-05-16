d498314598ca2eef5187534f1f3c06ed7c08945f
Greg Donnell <gregory.donnell74@gmail.com>
2018-05-16 06:49:04 -0600

    End of S2 L19

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -14,10 +14,19 @@ public class NumberWizards : MonoBehaviour {
 
         print("The highest number you can pick is " + max);
         print("The lowest number you can pick is " + min);
+
+        print("Is the number higher or lower than 500?");
+        print("Up arrow for higher, down arrow for lower, return for equal");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            print("Up arrow pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            print("Down arrow pressed");
+        }
+    }
 }
